eaa263014518994c2aa4ff34a2fa75ce5fe5bebc
Matt DelSordo <mattdelsordo@gmail.com>
2018-06-06 15:59:53 -0400

    Add basic css

--- breakable_toy/demo/index.js
+++ breakable_toy/demo/index.js
@@ -3,11 +3,16 @@
 document.addEventListener('DOMContentLoaded', function onload() {
     // create map object
     var map = L.map('map', {
-        center: [41.2033, -77.1945], zoom: 7
+        center: [41, -77.5], zoom: 7
     })
 
     // add basemap
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap' }).addTo(map)
+    //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap' }).addTo(map)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
 
     // add garden map
     L.tileLayer('http://localhost:5000/database/postgres/table/pa_gardens/{z}/{x}/{y}.png').addTo(map)
