f28c8e827d7f50980662e89b7a206b00745a9275
Matt DelSordo <mattdelsordo@gmail.com>
2018-06-14 12:11:23 -0400

    Improve meaningfulness of script names and fix script bugs
    Fixes bug where script setup would prevent deployment while
    running in development mode. Also improves console script
    syntax.

--- node_lambnik/src/tiler/src/api.js
+++ node_lambnik/src/tiler/src/api.js
@@ -2,9 +2,12 @@ import API from 'claudia-api-builder'
 
 const api = new API()
 
-api.get('/', () => 'Hello!')
+api.get('/', () => 'Hello, world!')
 
-api.get('/greet/{name}', req => `Top 'o the mornin to ye, ${req.pathParams.name}.`)
+api.get(
+    '/greet/{name}',
+    req => `Top 'o the mornin to ye, ${req.pathParams.name}.`,
+)
 
 // not es6-ic, but necessary for claudia to find the api
 module.exports = api
