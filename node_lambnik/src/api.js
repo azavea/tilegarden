bf52029591574387c64a64d392fda1826db57462
Matt DelSordo <mattdelsordo@gmail.com>
2018-06-14 09:50:23 -0400

    Add local testing server

--- node_lambnik/src/api.js
+++ node_lambnik/src/api.js
@@ -2,7 +2,9 @@ import API from 'claudia-api-builder'
 
 const api = new API()
 
-api.get('/', () => 'Hello')
+api.get('/', () => 'Hello!')
+
+api.get('/greet/{name}', (req) => `Top 'o the mornin to ye, ${req.pathParams.name}.`)
 
 // not es6-ic, but necessary for claudia to find the api
 module.exports = api
