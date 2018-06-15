667d267f3eeae592eaf3b6e0207323f2ba144d0d
Matt DelSordo <mattdelsordo@gmail.com>
2018-06-15 16:32:02 -0400

    Bookmark where we're at with getting image display to work
    Former-commit-id: a0d1f1acc115e14356c8d723a9f10462bb0f6d5a

--- node_lambnik/src/tiler/src/api.js
+++ node_lambnik/src/tiler/src/api.js
@@ -1,12 +1,38 @@
-import API from 'claudia-api-builder'
+/**
+ * Entrypoint for APIGateway
+ */
 
-const api = new API()
+import APIBuilder from 'claudia-api-builder'
+import fs from 'fs'
+import util from 'util'
+import path from 'path'
+
+const promiseReadFile = util.promisify(fs.readFile)
+
+const api = new APIBuilder()
 
 api.get('/', () => 'Hello, world!')
 
+api.get('/greet/{name}', request => `Hello, ${request.pathParams.name}!`)
+
+api.get('/echo', request => request)
+
 api.get(
-    '/greet/{name}',
-    req => `Top 'o the mornin to ye, ${req.pathParams.name}.`,
+    '/page.html',
+    () => promiseReadFile(path.join(__dirname, 'page.html'), 'utf-8'),
+    { success: { contentType: 'text/html' } },
+)
+
+api.setBinaryMediaTypes(['image/jpeg'])
+api.get(
+    '/img.jpg',
+    () => promiseReadFile(path.join(__dirname, 'img.jpg')),
+    {
+        success: {
+            contentType: 'image/jpeg',
+            contentHandling: 'CONVERT_TO_BINARY',
+        },
+    },
 )
 
 // not es6-ic, but necessary for claudia to find the api
