d02268b342b3b2176ebcc37224b7adaae58a5a1a
Matt DelSordo <mattdelsordo@gmail.com>
2018-06-20 10:37:58 -0400

    Establish connection with PostGIS

--- node_lambnik/src/tiler/src/_mapnik-hello-world.js
+++ node_lambnik/src/tiler/src/_mapnik-hello-world.js
@@ -3,20 +3,18 @@
  * https://github.com/mapnik/node-mapnik-sample-code/blob/master/tile/database/app.js
  */
 import mapnik from 'mapnik'
-import mercator from '@mapbox/sphericalmercator'
+import SphericalMercator from '@mapbox/sphericalmercator'
 import path from 'path'
 
 const POSTGIS_SETTINGS = {
     host: process.env.POSTGRES_HOST,
     port: '5432',
     dbname: process.env.POSTGRES_DB,
-    table: 'pwd_inlets',
+    table: 'pa_gardens',
     user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    type : 'raster',
+    type : 'postgis',
     geometry_field: 'geom',
     initial_size: '10', // TODO: what does this do?
-    estimate_extent: '1', // TODO: what does THIS do?
 }
 
 const getDatasource = () => {
@@ -34,8 +32,10 @@ export const serveTile = (z, y, x) => {
     const epsg3857 = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs' // noqa
     const map = new mapnik.Map(256, 256, epsg3857)
     const layer = new mapnik.Layer('tile', epsg3857)
-    const postgis = new mapnik.Datasource(getDatasource())
-    const bbox = mercator.xyz_to_envelope(x, y, z, false)
+    const postgis = getDatasource()
+    const bbox = new SphericalMercator().bbox(x, y, z)
+
+    console.log('Successfully defined constants.')
 
     layer.datasource = postgis
     layer.styles = ['point']
@@ -51,7 +51,9 @@ export const serveTile = (z, y, x) => {
         _map.render(img, (err, _img) => {
             if (err) throw err
 
-            return _img.encodeSync('png')
+            const encoded = _img.encodeSync('png')
+            console.log(`Encoded image: ${encoded}`)
+            return encoded
         })
     })
 }
