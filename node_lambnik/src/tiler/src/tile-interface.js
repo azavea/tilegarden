320b78e2b4b594594def2c60801804bdc8514360
Matt DelSordo <mattdelsordo@gmail.com>
2018-06-21 17:27:54 -0400

    Fix issues that prevented lambdas from deploying
    Former-commit-id: e9ad36e03d11231794fe28481e90703952162cfb

--- node_lambnik/src/tiler/src/tile-interface.js
+++ node_lambnik/src/tiler/src/tile-interface.js
@@ -5,32 +5,24 @@
  * TODO: this module could use a better name?
  */
 
-import Bunyan from 'bunyan'
 import path from 'path'
 import fs from 'fs'
 
 import { image, grid } from './tiler'
 
-// Configure logging
-const log = Bunyan.createLogger({ name: 'Tilegarden' })
-
 
 // TODO: flesh out how errors are handled
 const handleException = (e) => {
-    log.error(e)
     return e.toString()
 }
 
 // default landing page response
 // TODO: have this return usage instructions?
 export const home = () => {
-    log.debug('Hello, world!')
     return 'Hello, world!'
 }
 
 export const getGrid = async (req) => {
-    log.debug('Serving grid')
-
     try {
         // Handle url params
         const inlet = req.queryString.inlet
@@ -46,14 +38,14 @@ export const getGrid = async (req) => {
 }
 
 export const getImage = async (req) => {
-    log.debug('Serving tile')
-
     try {
         // Handle url params
         const inlet = req.queryString.inlet
         const z = Number(req.pathParams.z)
         const x = Number(req.pathParams.x)
-        const y = Number(req.pathParams.y)
+        // strip .png off of y
+        const preY = req.pathParams.y
+        const y = Number(preY.substr(0, preY.lastIndexOf('.')) || preY)
 
         // get tile buffer
         const tile = await image(z, x, y, inlet)
