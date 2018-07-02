6591fcb714db563d8efae71c2cf2cdce14aa815f
Matt DelSordo <mattdelsordo@gmail.com>
2018-07-02 17:00:08 -0400

    Make utf fields a query string rather than a path param
    Former-commit-id: 515980a59315e7e86fade62921ca15bda8d4d339

--- node_lambnik/src/tiler/src/path-config.js
+++ node_lambnik/src/tiler/src/path-config.js
@@ -4,6 +4,6 @@
 
 export const USAGE_PATH = '/'
 export const TILE_PATH = '/tile/{z}/{x}/{y}'
-export const GRID_PATH = '/grid/{infocolumn}/{z}/{x}/{y}'
+export const GRID_PATH = '/grid/{z}/{x}/{y}'
 export const FAVICON_PATH = '/favicon.ico'
 export const IMAGE_TEST_PATH = '/img'
