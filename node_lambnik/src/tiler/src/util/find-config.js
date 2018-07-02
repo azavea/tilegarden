7524bd56ec36e0c8263467a7885041501fe71472
Matt DelSordo <mattdelsordo@gmail.com>
2018-07-02 12:55:01 -0400

    Revert previous MML configuration setup to implement new system
    Former-commit-id: 410d682c475fdad40ac6c0720a2bd05f8c9206fb

--- node_lambnik/src/tiler/src/util/find-config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Dynamically provides path to mapnik config file based on current NODE_ENV
- */
-
-import path from 'path'
-
-export default () => {
-    return path.join(__dirname, `../config/map-config.${(process.env.NODE_ENV === 'development') ? 'dev' : 'prod'}.mml`)
-}
