8cfbf1187b2bc435e714ec191dff757ffc368bc5
Matt DelSordo <mattdelsordo@gmail.com>
2018-07-03 15:55:51 -0400

    Improve filtering documentation

--- node_lambnik/src/tiler/src/api.js
+++ node_lambnik/src/tiler/src/api.js
@@ -82,7 +82,8 @@ api.get(
                 <h2>Tilegarden Usage:</h2>
                 <ul>
                     <li>Render tile at zoom/x/y: <code>/tile/{z}/{x}/{y}.png</code></li>
-                    <li>UTF grid at zoom/x/y: <code>/grid/{z}/{x}/{y}</code></li>
+                    <li>UTF grid at zoom/x/y: <code>/grid/{z}/{x}/{y}?utfFields=field1,field2,field...N</code></li>
+                    <li>Filter layers: add <code>?layers=layer1,layer2,layer...N</code></li>
                 </ul>
                 <a href="https://github.com/azavea/tilegarden">See on GitHub</a>
             </body>
