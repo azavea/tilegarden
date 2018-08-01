97edbe3742aba7ae7a78a9f5a2f4c2b26d91a253
Matt DelSordo <mattdelsordo@gmail.com>
2018-08-01 16:22:43 -0400

    Delegate Carto logic to bash instead of node
    The Carto CLI is significantly simpler than the Node SDK. Moving
    the call to Carto into a bash script should streamline the build
    process when building with external files.

--- src/tiler/scripts/build-xml.sh
+++ src/tiler/scripts/build-xml.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+# This script is never meant to be called directly by the user.
+# It should only be called by other scripts that pass MML strings
+# in to it, so that different environments can have access to the
+# version of carto installed on the docker container.
+
+function main() {
+	tempFile="${1%%.*}.temp.mml"
+
+	# fill in environment variables
+	node scripts/template-vars.js "${2}" > ${tempFile}
+
+	# compile with carto
+	carto ${tempFile}
+
+	# clean up
+	rm ${tempFile}
+}
+
+main "${1}" "${2}"
