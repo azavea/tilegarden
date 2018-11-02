dbfc058b6dcfd5c5b5e4108c55853277ffa9b052
Klaas Hoekema <khoekema@azavea.com>
2018-11-02 10:06:05 -0400

    Add S3 cache between CloudFront proxy and API
    Adds configuration for an S3 bucket, configured as a website, to serve
    as a tile cache. Rather than sending requests right to the API, the
    CloudFront distribution will now request them from the S3 endpoint, which
    in turn will redirect to the API for tiles it doesn't have.
    
    Still to do: add code to the Lambda function to write generated tiles
    back to S3.

--- src/terraform/cloudfront.tf
+++ src/terraform/cloudfront.tf
@@ -7,28 +7,24 @@ provider "aws" {
 }
 
 output "domain" {
-  value = "${aws_cloudfront_distribution.tilegarden_test.domain_name}"
+  value = "${aws_cloudfront_distribution.tilegarden_proxy.domain_name}"
 }
 
-resource "aws_cloudfront_distribution" "tilegarden_test" {
+resource "aws_cloudfront_distribution" "tilegarden_proxy" {
   origin {
-    custom_origin_config {
-      http_port = 80
-      https_port = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols = ["TLSv1.2", "TLSv1.1", "TLSv1"]
-      origin_keepalive_timeout = 5
-      origin_read_timeout = 30
-    }
-    domain_name = "${var.source_id}.execute-api.${var.region}.amazonaws.com"
-    origin_path = "/latest"
-    origin_id = "${var.source_name}"
+    origin_id = "${local.cache_bucket_name}"
+    domain_name = "${aws_s3_bucket.tile_cache.website_endpoint}"
     custom_header {
       name = "Accept"
       value = "image/png"
     }
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
-
 
   price_class = "PriceClass_100"
   enabled = true
@@ -38,7 +34,7 @@ resource "aws_cloudfront_distribution" "tilegarden_test" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.source_name}"
+    target_origin_id = "${local.cache_bucket_name}"
 
     forwarded_values {
       query_string = true
---
+++ src/terraform/s3.tf
@@ -0,0 +1,55 @@
+locals {
+  cache_bucket_name = "${lower("${var.source_name}")}-cache"
+  api_hostname = "${var.source_id}.execute-api.${var.region}.amazonaws.com"
+}
+
+resource "aws_s3_bucket" "tile_cache" {
+  bucket = "${local.cache_bucket_name}"
+  acl = "public-read"
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+    routing_rules = <<EOF
+    [{
+      "Condition": {
+        "HttpErrorCodeReturnedEquals": "404"
+      },
+      "Redirect": {
+        "HostName" : "${local.api_hostname}",
+        "HttpRedirectCode" : "302",
+        "Protocol" : "https",
+        "ReplaceKeyPrefixWith": "latest/"
+      }
+    }]
+EOF
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "tile_cache" {
+  comment = "Created for ${var.source_name}"
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.tile_cache.arn}",
+      "${aws_s3_bucket.tile_cache.arn}/*"
+    ]
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "tile_cache" {
+  bucket = "${aws_s3_bucket.tile_cache.id}"
+  policy = "${data.aws_iam_policy_document.s3.json}"
+}
+
