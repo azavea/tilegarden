e7cd085412e3483159b930da94b002aec6966a8c
Klaas Hoekema <khoekema@azavea.com>
2019-06-10 22:32:33 -0400

    Switch to compiling config XML in advance but filling vars at runtime
    Changes how map config is handled:
    - Makes the Mapnik XML canonical, and tracked.
    - Moves the Carto MML/MSS map config files into a subdirectory. They are
    now useful as working files, but aren't used by the running code.
    - Removes the compilation step from the `deploy` and `local` yarn commands.
    Config file compilation can still be run using the `build-xml` script, but
    won't be run automatically.
    - Removes the 'template-vars' function that was being used during XML
    compilation, moving the logic into a function in tiler.js. The XML files
    will now be templates to be filled with variables from the environment,
    like the Carto map config files used to be, and the substitutions will
    be made at runtime.
    - Changes the variables and how they're provided to docker-compose to
    support the new setup of filling them in at runtime. This includes adding
    commands to the `deploy` scripts to add the variables to the Lambda
    function's environment.

--- src/tiler/src/tiler.js
+++ src/tiler/src/tiler.js
@@ -79,6 +79,17 @@ async function fetchMapFile(options) {
     }
 }
 
+/* Substitutes environment variables into a string using a basic regex-driven template syntax.
+ *
+ * Any occurrence of ${ENV_VAR} will be replaced with the value of that environment variable.
+ */
+function fillVars(xmlString) {
+    return xmlString.replace(
+        /\$\{([A-Z0-9_]+)\}/g,
+        (_, envName) => `${process.env[envName]}`,
+    )
+}
+
 /**
  * Creates a map based on configured datasource and style information
  */
@@ -91,6 +102,7 @@ async function createMap(mapConfig) {
     // Load map specification from xml string and apply some transforms and filters
     try {
         return await fetchMapFile(configOptions)
+            .then(fillVars)
             .then(parseXml)
             .then(xmlJsObj => filterVisibleLayers(xmlJsObj, layers))
             .then(buildXml)
